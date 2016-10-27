ab89d1594601f28c60b4716b92f68d9cedf924a8
Amit Saini <amit.saini@mail.vinove.com>
2016-10-27 12:05:24 +0530

    (initial commit)

---
+++ TrendingGitSwift/Model/Project.swift
@@ -0,0 +1,13 @@
+//
+//  Project.swift
+//  TrendingGitSwift
+//
+//  Created by Varun Tyagi on 27/10/16.
+//  Copyright © 2016 Varun Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class Project: NSObject {
+
+}
---
+++ TrendingGitSwift/TableViewCell/ProjectListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectListTableViewCell.swift
+//  TrendingGitSwift
+//
+//  Created by Varun Tyagi on 27/10/16.
+//  Copyright © 2016 Varun Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ProjectListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
